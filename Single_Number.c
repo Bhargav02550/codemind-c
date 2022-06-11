73d1e58b1fa87ad8dd757137bde52aad79ab81e3
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-06-11 11:17:24 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int r=a[0];     for(i=1;i<n;i++)     {         r=r^a[i];     }     printf("%d",r); }

--- Single_Number.c
+++ Single_Number.c
@@ -1,26 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j,c,a[i];
+    int n,i;
     scanf("%d",&n);
+    int a[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<n;i++)
+    int r=a[0];
+    for(i=1;i<n;i++)
     {
-        c=0;
-        for(j=0;j<n;j++)
-        {
-            if((a[i]==a[j])&&(j!=i))
-            {
-                c++;
-            }
-        }
-        if(c==0)
-        {
-            printf("%d",a[i]);
-            break;
-        }
+        r=r^a[i];
     }
+    printf("%d",r);
 }
